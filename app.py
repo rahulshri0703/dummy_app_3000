27076696609b6d2a2e800a738b67fa553689ce8f
rahulshri0703 <rahulshri0703@gmail.com>
2023-05-24 18:12:00 +0530

    6

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return "This is INDEX FIRST PAGE OF AWS"
+
+
+@app.get("/health")
+def health():
+    return {"health": "ok"}
