be2f052b6c62f06d10d4bf63ee66f7c11e62da87
rahulshri0703 <rahulshri0703@gmail.com>
2023-05-24 18:10:06 +0530

    6

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.get('/')
-def index():
-    return "This is an index page"
 
+@app.get("/")
+def index():
+    return "This is INDEX FIRST PAGE OF AWS"
